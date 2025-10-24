f2bd94c5548f0d2646c03820f508bdd9e8d8ae83
swag epic man <jonnofmark@gmail.com>
2025-10-24 13:14:35 -0600

    finalized the fishing mechanic aww man

---
+++ 12-gauge-fishing-rod/fishing/minigame/fishing_game_2.gd
@@ -0,0 +1,24 @@
+extends Node
+
+signal fish(strength: float)
+
+@onready var area_2d: RigidBody2D = $Area2D
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+@onready var ray_cast_2d_2: RayCast2D = $RayCast2D2
+
+var targeted:= false
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed(&"Primary"):
+		if ray_cast_2d.is_colliding():
+			area_2d.linear_velocity = Vector2.ZERO
+			area_2d.apply_central_impulse(Vector2.UP * 525.0)
+
+func _physics_process(delta: float) -> void:
+	if ray_cast_2d_2.is_colliding():
+		fish.emit(area_2d.linear_velocity.length())
+		return
+
+	if ray_cast_2d.is_colliding():
+		area_2d.modulate = Color.GREEN
+	else:
+		area_2d.modulate = Color.WHITE
--- 12-gauge-fishing-rod/fishing/minigame/fishing_minigame.gd
+++ 12-gauge-fishing-rod/fishing/minigame/fishing_minigame.gd
@@ -27,15 +27,15 @@ func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector(&"Left",&"Right",&"Forward",&"Backward")
 	if direction:
 		player_reel.apply_central_force(direction * 150.0)
-		
-	
+
+
 	if detected:
 		progress_bar.value += 25.0 * delta
 		texture_rect.modulate = lerp(texture_rect.modulate,Color.GREEN,5.0 * delta)
 	else:
 		progress_bar.value -= 15.0 * delta
 		texture_rect.modulate = lerp(texture_rect.modulate,Color.RED,1.0 * delta)
-	
+
 	if progress_bar.value >= progress_bar.max_value:
 		hide()
 func _on_fish_point_body_entered(body: Node2D) -> void:
@@ -49,14 +49,14 @@ func _on_timer_timeout() -> void:
 	var new_position:= fish_point.position + Vector2(randf_range(-125,125),randf_range(-125,125))
 	if new_position.x >= 512.0 or new_position.x <= 0.0:
 		new_position.x = fish_point.position.x
-	
+
 	if new_position.y >= 512.0 or new_position.y <= 0.0:
 		new_position.y = fish_point.position.y
 	if point_tween:
 		point_tween.kill()
 	point_tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
 	point_tween.tween_property(fish_point,^"position",new_position	,randf_range(0.5,5.0))
-	
+
 func tween_fish() -> void:
 	fish_sprite.scale = Vector2.ONE * 0.15
 	if fish_tween:
